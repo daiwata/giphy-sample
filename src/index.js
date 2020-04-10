165fae4cbd33db448b8f72b82511181b04066eb9
daiwata <watanabedai5427@gmail.com>
2020-04-10 11:22:51 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import { render } from '@testing-library/react';
+import { Search } from './components/Search'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    constructor() {
+        super();
+        this.state = { gifUrlList: [] }
+    }
+
+    renderImageList(list) {
+        const imageList = list.map(url => <img src={url} width="200px" />)
+        return imageList
+    }
+
+    render() {
+        return (
+            <div>
+                <Search search={this.giphyApi} />
+                {this.renderImageList(this.state.gifUrlList)}
+            </div>
+        )
+    }
+
+    giphyApi = (target) => {
+        const search = target
+        const key = 'fjkFSdzcC0ZSKJ26mGAn5uuDgSQhxthQ'
+        const limit = 100
+
+        const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`
+
+        axios.get(url).then(res => {
+            const data = res.data.data
+            const imageUrlList = data.map(item => item.images.downsized.url)
+            this.setState({ gifUrlList: imageUrlList })
+        })
+    }
+}
+
+render(
+    <App />, document.getElementById('root')
+)
